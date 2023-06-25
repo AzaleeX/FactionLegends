a065702074b5e4d8490a66315b64c2d2b1f7a09a
Azalee <azaleemcpe@gmail.com>
2023-06-25 11:04:46 +0200

    add my brand

--- src/Azalee/FactionLegends/API/FactionLegendsAPI.php
+++ src/Azalee/FactionLegends/API/FactionLegendsAPI.php
@@ -1,5 +1,19 @@
 <?php
 
+/***
+ *   _____          _   _             _                              _
+ *  |  ___|_ _  ___| |_(_) ___  _ __ | |    ___  __ _  ___ _ __   __| |___
+ *  | |_ / _` |/ __| __| |/ _ \| '_ \| |   / _ \/ _` |/ _ \ '_ \ / _` / __|
+ *  |  _| (_| | (__| |_| | (_) | | | | |__|  __/ (_| |  __/ | | | (_| \__ \
+ *  |_|  \__,_|\___|\__|_|\___/|_| |_|_____\___|\__, |\___|_| |_|\__,_|___/
+ *                                              |___/
+ * @author AzaleeX
+ * @link https://github.com/AzaleeX
+ *
+ * use Wiki for use FactionLegendsAPI on https://github.com/AzaleeX/FactionLegends/wiki
+ *
+ */
+
 namespace Azalee\FactionLegends\API;
 
 use pocketmine\utils\SingletonTrait;
@@ -9,5 +23,4 @@ class FactionLegendsAPI
     use SingletonTrait;
 
 
-    
 }
--- src/Azalee/FactionLegends/Utils/QuerysInterface.php
+++ src/Azalee/FactionLegends/Utils/QuerysInterface.php
@@ -1,9 +1,23 @@
 <?php
 
+/***
+ *   _____          _   _             _                              _
+ *  |  ___|_ _  ___| |_(_) ___  _ __ | |    ___  __ _  ___ _ __   __| |___
+ *  | |_ / _` |/ __| __| |/ _ \| '_ \| |   / _ \/ _` |/ _ \ '_ \ / _` / __|
+ *  |  _| (_| | (__| |_| | (_) | | | | |__|  __/ (_| |  __/ | | | (_| \__ \
+ *  |_|  \__,_|\___|\__|_|\___/|_| |_|_____\___|\__, |\___|_| |_|\__,_|___/
+ *                                              |___/
+ * @author AzaleeX
+ * @link https://github.com/AzaleeX
+ */
+
 namespace Azalee\FactionLegends\Utils;
 
 interface QuerysInterface
 {
+
+    //INIT
+
     public const FactionLegends_Faction_Init = "factionlegends.faction.init";
     public const FactionLegends_Player_Init = "factionlegends.player.init";
     public const FactionLegends_Home_Init = "factionlegends.home.init";
